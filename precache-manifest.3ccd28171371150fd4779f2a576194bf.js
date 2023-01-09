def61141567a432e1c6fa011cab473b00b7313df
Alexis Attardo <AlexisAtt@users.noreply.github.com>
2023-01-09 16:15:10 -0500

    Updates

---
+++ precache-manifest.3ccd28171371150fd4779f2a576194bf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1aa7ab36b1daa926475",
+    "url": "/Alexis-Att/static/css/main.c040d922.chunk.css"
+  },
+  {
+    "revision": "d1aa7ab36b1daa926475",
+    "url": "/Alexis-Att/static/js/main.d1aa7ab3.chunk.js"
+  },
+  {
+    "revision": "e04cd450e9824253c1bd",
+    "url": "/Alexis-Att/static/js/1.e04cd450.chunk.js"
+  },
+  {
+    "revision": "099570883095050ea598",
+    "url": "/Alexis-Att/static/css/2.4029f111.chunk.css"
+  },
+  {
+    "revision": "099570883095050ea598",
+    "url": "/Alexis-Att/static/js/2.09957088.chunk.js"
+  },
+  {
+    "revision": "848fdc760703207924dd",
+    "url": "/Alexis-Att/static/js/runtime~main.848fdc76.js"
+  },
+  {
+    "revision": "4325eb0f34548b9f7cc425c36f268f96",
+    "url": "/Alexis-Att/static/media/mailz.4325eb0f.jpeg"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/Alexis-Att/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/Alexis-Att/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/Alexis-Att/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/Alexis-Att/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/Alexis-Att/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/Alexis-Att/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/Alexis-Att/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "6444890b81ccb5f14b4cbfc9e2d792e3",
+    "url": "/Alexis-Att/static/media/profile-image.6444890b.jpeg"
+  },
+  {
+    "revision": "06068aaa4f6de4b013d1968127d31148",
+    "url": "/Alexis-Att/static/media/bg.06068aaa.png"
+  },
+  {
+    "revision": "fb92c1c868bc1efd8abd08e05ca9039b",
+    "url": "/Alexis-Att/static/media/pic2.fb92c1c8.jpeg"
+  },
+  {
+    "revision": "f17b17eaf6cf60ec29b97c78afd4edbf",
+    "url": "/Alexis-Att/static/media/education.f17b17ea.svg"
+  },
+  {
+    "revision": "77ae1b679453f8681c04d59c827838ce",
+    "url": "/Alexis-Att/static/media/interests.77ae1b67.svg"
+  },
+  {
+    "revision": "0544df6cbc5b3a8844bce4aeb266e9f6",
+    "url": "/Alexis-Att/static/media/programming-skills.0544df6c.svg"
+  },
+  {
+    "revision": "4940b2023affe10af0ba092555bf3e6c",
+    "url": "/Alexis-Att/static/media/projects.4940b202.svg"
+  },
+  {
+    "revision": "a59c5d12dbc74e8698f847a01bca48c0",
+    "url": "/Alexis-Att/static/media/work-history.a59c5d12.svg"
+  },
+  {
+    "revision": "deace2279f7bc04433b1963d5c195c46",
+    "url": "/Alexis-Att/static/media/bg-1.deace227.jpg"
+  },
+  {
+    "revision": "34735164763fd78a5cfd5582f73ec6da",
+    "url": "/Alexis-Att/index.html"
+  }
+];
